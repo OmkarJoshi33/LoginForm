63b0dfa780e378b28b69845fb209400c7965f4c0
omkar <omkarjoshi1231@gmail.com>
2024-12-21 23:38:38 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Animatedbg from "./component/Animatedbg.jsx";
+import './component/animatedbg.css';
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+  <Animatedbg />
+    <App />
+  </>
+);
